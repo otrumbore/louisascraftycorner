69a986966a6c38779f9cdeca2cebe59c3101b0ff
Odnel <otrumbore@gmail.com>
2024-01-12 15:54:20 -0500

    changing host ip for start

--- frontend/src/api/admin/users.api.js
+++ frontend/src/api/admin/users.api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const getUsers = async () => {
 	const API_URL = import.meta.env.VITE_SERVER_API_URL;
 	try {
-		const response = await axios.get(`${API_URL}/user/getUsers`);
+		const response = await axios.get(`${API_URL}/api/user/getUsers`);
 		return response.data.data;
 	} catch (error) {
 		console.log(error);
