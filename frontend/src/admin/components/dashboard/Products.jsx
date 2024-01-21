4f1fbacd0b3cf6a50267ad6f64f4039fe914fe0a
Odnel <otrumbore@gmail.com>
2024-01-21 11:48:01 -0500

    pagination added

--- frontend/src/admin/components/dashboard/Products.jsx
+++ frontend/src/admin/components/dashboard/Products.jsx
@@ -9,6 +9,7 @@ import {
 	MdOutlineEdit,
 	MdOutlinePreview,
 } from 'react-icons/md';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import { VscPreview } from 'react-icons/vsc';
 import { Link } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
@@ -17,8 +18,11 @@ import DefaultProductImg from '../../../assets/product-img/default.png';
 
 const Products = ({ archived }) => {
 	const [products, setProducts] = useState([]);
+	const [allProducts, setAllProducts] = useState([]);
 	const [loading, setLoading] = useState(true);
 	const { enqueueSnackbar } = useSnackbar();
+	const [currentPage, setCurrentPage] = useState(1);
+	const [productsPerPage] = useState(10);
 	const [searchQuery, setSearchQuery] = useState('');
 
 	const [showModal, setShowModal] = useState(null);
@@ -60,14 +64,16 @@ const Products = ({ archived }) => {
 					product.name.toLowerCase().includes(searchQuery.toLowerCase()) // Check if name includes the search query (case-insensitive)
 			);
 
-			//console.log(filteredProducts);
-			console.log(products);
-			setProducts(filteredProducts);
-			setLoading(false); // Update loading state when data is fetched
-			//console.log(filteredProducts);
+			const indexOfLastProduct = currentPage * productsPerPage;
+			const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+			const currentProducts = filteredProducts.slice(
+				indexOfFirstProduct,
+				indexOfLastProduct
+			);
+			setAllProducts(filteredProducts);
+			setProducts(currentProducts);
 		} catch (error) {
 			console.error(error);
-			setLoading(false); // Update loading state in case of error
 			return (
 				<div className='mt-8 w-full justify-center text-xl'>
 					Could not load products refresh to try again.
@@ -77,6 +83,8 @@ const Products = ({ archived }) => {
 			setLoading(false);
 		}
 	};
+
+	const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
 	const sendProductUpdate = async (id, data, name) => {
 		setLoading(true);
@@ -125,7 +133,7 @@ const Products = ({ archived }) => {
 
 	useEffect(() => {
 		fetchProducts(archived, searchQuery);
-	}, [archived, showModal, searchQuery]);
+	}, [archived, showModal, searchQuery, currentPage, productsPerPage]);
 
 	const adminNotifications = [
 		{ text: '111111 - low inventory' },
@@ -133,6 +141,12 @@ const Products = ({ archived }) => {
 		{ text: '111111 - low inventory' },
 		{ text: '111112 - low inventory' },
 	];
+
+	const pageNumbers = [];
+	for (let i = 1; i <= Math.ceil(allProducts.length / productsPerPage); i++) {
+		pageNumbers.push(i);
+	}
+
 	// Calculate counts of active and inactive products
 	const activeCount = products.filter(
 		(product) => product.active === true
@@ -161,14 +175,17 @@ const Products = ({ archived }) => {
 						<p>Active: {activeCount}</p>
 						<p>Inactive: {inactiveCount}</p>
 					</div>
-					<div className='hidden lg:flex lg:w-[40%] p-4 border-4 border-primary border-dashed items-center justify-between rounded-md'>
+					<div className='flex flex-col lg:flex-row lg:w-[40%] lg:my-0 p-4 border-4 border-primary border-dashed items-center justify-between rounded-md mb-5'>
 						<input
 							type='text'
-							className='input p-2 w-[50%]'
+							className='input p-2 lg:w-[50%]'
 							placeholder='Search...'
 							onChange={handleChange}
 						/>
-						<Link to={'/admin/addproduct'} className='btn-outline px-0 py-0'>
+						<Link
+							to={'/admin/addproduct'}
+							className='hidden lg:block btn-outline px-0 py-0'
+						>
 							<MdAdd className='text-5xl' />
 						</Link>
 					</div>
@@ -327,6 +344,51 @@ const Products = ({ archived }) => {
 						</tbody>
 					</table>
 				</div>
+				{/* Pagination */}
+				<div className='flex justify-center mt-4'>
+					<ul className='flex gap-2'>
+						{currentPage > 1 && (
+							<li className=''>
+								<button
+									onClick={() => paginate(currentPage - 1)}
+									className='btn-outline px-2'
+								>
+									<FaChevronLeft size={25} />
+								</button>
+							</li>
+						)}
+
+						{pageNumbers.map((number) => (
+							<li
+								key={number}
+								//className={}
+							>
+								<button
+									onClick={() => paginate(number)}
+									className={` ${
+										number === currentPage
+											? 'btn px-4'
+											: 'btn-outline py-2 px-4'
+									}`}
+								>
+									{number}
+								</button>
+							</li>
+						))}
+
+						{currentPage < pageNumbers.length && (
+							<li className=''>
+								<button
+									onClick={() => paginate(currentPage + 1)}
+									className='btn-outline px-2'
+								>
+									<FaChevronRight size={25} />
+								</button>
+							</li>
+						)}
+					</ul>
+				</div>
+
 				{showModal && <ProductModal product={showModal} onClose={closeModal} />}
 			</div>
 		</>
