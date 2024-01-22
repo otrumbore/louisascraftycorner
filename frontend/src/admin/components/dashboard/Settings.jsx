80b97613d16a452b080446d3fd853df8c14a345a
Odnel <otrumbore@gmail.com>
2024-01-22 11:58:03 -0500

    removed logging changed some UI elements

--- frontend/src/admin/components/dashboard/Settings.jsx
+++ frontend/src/admin/components/dashboard/Settings.jsx
@@ -17,7 +17,6 @@ const Settings = () => {
 			const fetchedSettings = await getSettings();
 			setCollections(fetchedSettings.collections);
 			setWebsiteBanner(fetchedSettings.website_banner);
-			console.log(fetchedSettings);
 		} catch (error) {
 			console.error(error);
 		} finally {
@@ -28,7 +27,6 @@ const Settings = () => {
 	const sendUpdateSettings = async (updatedData) => {
 		setLoading(true);
 		try {
-			console.log(updatedData);
 			await updateSetting(updatedData);
 			fetchSettings();
 		} catch (error) {
@@ -128,40 +126,29 @@ const Settings = () => {
 			<div className='mt-4 flex flex-col-reverse lg:flex-row justify-between items-start gap-4'>
 				<div className='border-4 w-full border-primary p-4 rounded-md lg:w-[50%]'>
 					<h3 className='text-xl border-b-4 pb-2 mb-2'>Main Settings</h3>
-					<div className='flex flex-col gap-2'>
-						<div className='flex w-full gap-2 items-center'>
-							<h4 className='text-xl w-[25%]'>Website Banner</h4>
+					<div className='grid grid-flow-row gap-4'>
+						<div className='grid items-center grid-cols-[1fr,2fr]'>
+							<h4 className='text-xl'>Banner</h4>
 							<input
 								className='input py-1'
 								onChange={(e) => setWebsiteBanner(e.target.value)}
 								value={websiteBanner}
 							/>
 						</div>
-						<div>
-							<div className='flex mt-4 w-full gap-2 items-center'>
-								<h4 className='text-xl w-[20%]'>Toggles</h4>
-								<div className='itens-center'>
-									<input
-										class="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
-										type='checkbox'
-										role='switch'
-										checked
-										id='flexSwitchCheckDefault'
-									/>
-									<label
-										class='inline-block pl-[0.15rem] hover:cursor-pointer'
-										for='flexSwitchCheckDefault'
-									>
-										Reward System
-									</label>
-								</div>
+						<div className='grid items-center grid-cols-[1fr,2fr]'>
+							<h4 className='text-xl'>Rewards</h4>
+							<div className='itens-center'>
+								<select value={true} className='input py-2'>
+									<option value={true}>ON</option>
+									<option value={false}>OFF</option>
+								</select>
 							</div>
 						</div>
-						<div className='flex justify-end'>
-							<button className='btn' onClick={handleSaveSettings}>
-								Save
-							</button>
-						</div>
+					</div>
+					<div className='mt-4 flex justify-end'>
+						<button className='btn' onClick={handleSaveSettings}>
+							Save
+						</button>
 					</div>
 				</div>
 				<div className='border-4 w-full border-primary p-4 rounded-md lg:w-[50%]'>
@@ -172,7 +159,7 @@ const Settings = () => {
 					</div>
 					<div className='flex flex-col gap-2 justify-center'>
 						<div className='flex justify-between items-center'>
-							<h5 className=''>Collections:</h5>
+							<h4 className='text-xl'>Collections:</h4>
 							<select
 								className='px-4 py-1 w-fit border-4 border-primary rounded-md'
 								value={collectionSelect}
