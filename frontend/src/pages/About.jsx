d30f3e20ef108e6c238b1165fec1d833c2ba1fcf
Odnel <otrumbore@gmail.com>
2024-01-29 17:34:59 -0500

    typo from events to about

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -19,7 +19,7 @@ const About = () => {
 	return (
 		<div className='w-full flex justify-center min-h-[65vh]'>
 			<div className='p-4 w-full max-w-[1200px]'>
-				<h2 className='text-3xl font-semibold text-center mb-8'>Events</h2>
+				<h2 className='text-3xl font-semibold text-center mb-8'>About</h2>
 				<div className='flex justify-center '>
 					<div
 						className='w-[80%] p-4 border-4 border-primary rounded-md adminHTML'
