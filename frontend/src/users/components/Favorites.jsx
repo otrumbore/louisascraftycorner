ee50e4970173f8b040625575085870f3a0aff3c7
Odnel <otrumbore@gmail.com>
2024-02-03 10:55:34 -0500

    temp fave update

--- frontend/src/users/components/Favorites.jsx
+++ frontend/src/users/components/Favorites.jsx
@@ -1,16 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import DefaultProductImg from '../../assets/product-img/default.png';
 import { Link, useNavigate } from 'react-router-dom';
-import {
-	MdOutlineDeleteForever,
-	MdOutlineUpdateDisabled,
-	MdAddShoppingCart,
-} from 'react-icons/md';
+import { MdOutlineDeleteForever, MdAddShoppingCart } from 'react-icons/md';
 import { useUser } from '../../context/UserContext';
 import axios from 'axios';
 import LoadingModal from '../../components/LoadingModal';
-
-import SantaHat from '../../assets/product-img/santa-hat-ordiment.png';
+import DefaultProductImg from '../../assets/product-img/default.png';
 import { useSnackbar } from 'notistack';
 import { useCart } from '../../context/CartContext';
 
@@ -27,7 +21,6 @@ const Favorites = () => {
 		setLoading(true);
 		try {
 			if (userDetails) {
-				//console.log(userFavorites);
 				const favoriteProductsPromises = userFavorites.map(async (item) => {
 					try {
 						const favoriteProducts = await axios.get(
@@ -53,60 +46,46 @@ const Favorites = () => {
 				variant: 'error',
 			});
 			navigate('/user/dashboard#orders');
+		} finally {
+			setLoading(false);
 		}
 	};
 
 	useEffect(() => {
-		favorites.length <= 0 && getProducts();
-	}, []);
+		// Load favorites on component mount
+		if (favorites.length === 0) {
+			getProducts();
+		}
+	}, [favorites]);
 
 	useEffect(() => {
+		// Update favorites when userFavorites change
 		if (userFavorites.length !== favorites.length) {
 			getProducts();
-		} else {
-			setLoading(false);
 		}
 	}, [favorites, userFavorites]);
+
 	return (
 		<>
 			<LoadingModal loading={loading} />
-			<div className='w-full'>
+			<div className='w-full flex flex-col items-center'>
 				<div className='flex w-full justify-center items-center'>
 					<h3 className='text-2xl lg:text-3xl'>Your Favorites</h3>
 				</div>
 				{userFavorites.length <= 0 ? (
 					<div className='mt-8 text-center'>No favorites added yet. </div>
 				) : (
-					<div className='mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full gap-4'>
-						{favorites.length > 0 &&
-							favorites.map((item, i) => (
-								<div
-									className='border-2 border-primary p-4 rounded-lg mb-4'
-									key={i}
-								>
-									{item.data.name === 'deleted' ? (
-										<div className='flex h-full items-center p-4'>
-											<h4 className='text-2xl'>Item no longer for sale</h4>
-											<button
-												onClick={() => {
-													removeFromFavorites(item.data.storeId);
-													//getProducts();
-													enqueueSnackbar(
-														'Deleted ' + item.data.storeId + ' from favorites',
-														{
-															variant: 'success',
-														}
-													);
-												}}
-												className='btn-ghost text-red-600 hover:text-white p-2'
-											>
-												<MdOutlineDeleteForever className='text-3xl lg:text-4xl' />
-											</button>
-										</div>
-									) : (
-										<div className='flex items-center'>
-											<Link to={`/product/${item.data._id}`}>
-												<div className='flex h-[100px] w-[100px] items-center justify-center'>
+					<div className='mt-8 w-full overflow-x-auto '>
+						<div className='min-w-full bg-slate-100'>
+							<div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
+								{favorites.length > 0 &&
+									favorites.map((item, i) => (
+										<div
+											key={i}
+											className='p-4 flex flex-col gap-y-4 lg:gap-0 lg:flex-row lg:justify-between items-center border-4 border-primary rounded-md'
+										>
+											<div className='flex items-center w-full justify-between'>
+												<Link to={`/product/${item.data._id}`}>
 													<img
 														src={
 															item.data.image
@@ -119,72 +98,51 @@ const Favorites = () => {
 																: 'Default Product Image'
 														}
 														onError={(e) => {
-															e.target.src = DefaultProductImg; // Fallback to default image on error
+															e.target.src = DefaultProductImg;
 															e.target.alt = 'Default Product Image';
 														}}
-														className='object-cover border-2 border-slate-300 h-full w-full rounded-md'
+														className='object-cover h-16 w-16 rounded-md'
 													/>
-												</div>
-											</Link>
-											<div className='flex flex-col w-full items-center'>
-												<div className='pl-4 flex flex-col gap-y-1 w-full'>
-													<Link to={`/product/${item.data._id}`}>
-														<div className='flex flex-wrap font-bold text-lg'>
-															{item.data.name ? `${item.data.name} - ` : ''}
-															{item.data.type
-																? item.data.type.charAt(0).toUpperCase() +
-																  item.data.type.slice(1)
-																: ''}
+												</Link>
+												<div className=''>{item.data.name}</div>
+												<div>
+													{item.data.archived ? (
+														<p>Out of stock!</p>
+													) : item.data.sale > 0 ? (
+														<div className='flex gap-x-2'>
+															<p className='line-through mr-2'>
+																${item.data.price}
+															</p>
+															<p className='text-red-600'>${item.data.sale}</p>
 														</div>
-													</Link>
-													<div className='flex lg:text-lg'>
-														{item.data.archived ? (
-															<p>Out of stock!</p>
-														) : item.data.sale > 0 ? (
-															<>
-																<p className='line-through mr-2'>
-																	${item.data.price}
-																</p>
-																<p className='text-red-600'>
-																	${item.data.sale}
-																</p>
-															</>
-														) : (
-															<p>${item.data.price}</p>
-														)}
-													</div>
+													) : (
+														<p>${item.data.price}</p>
+													)}
 												</div>
+											</div>
 
-												<div className='mt-2 flex flex-row w-full items-center justify-end gap-2'>
-													{!item.data.active && <p>No longer available</p>}
-
-													<div className='flex gap-x-2 items-center'>
-														{/*<p className='lg:text-lg'>QTY: {item.qty}</p>
-												 <Link to={`/product/${item._id}`}>
-													<MdEdit className='text-blue-400 text-2xl' />
-												</Link> */}
+											<div className='flex w-full items-center justify-between lg:justify-end'>
+												{item.data.name === 'deleted' ? (
+													<p className='text-2xl'>Item no longer for sale</p>
+												) : (
+													<>
+														{!item.data.active && <p>No longer available</p>}
 														<button
 															onClick={() => {
 																removeFromFavorites(item.data.storeId);
-																//getProducts();
 																enqueueSnackbar(
 																	'Deleted ' +
-																		item.data.name +
+																		item.data.storeId +
 																		' from favorites',
-																	{
-																		variant: 'success',
-																	}
+																	{ variant: 'success' }
 																);
 															}}
-															className='btn-ghost text-red-600 hover:text-white p-2'
+															className='btn-ghost px-2 text-red-600 hover:text-white ml-4'
 														>
 															<MdOutlineDeleteForever className='text-2xl lg:text-3xl' />
 														</button>
-													</div>
-
-													<div className='flex gap-x-6 lg:gap-x-4 items-center justify-center'>
 														<button
-															className={`has-tooltip px-4 py-2 btn ${
+															className={`btn px-4 py-2 ml-4 ${
 																cartItems.some(
 																	(cartItem) => cartItem._id === item.data._id
 																) ||
@@ -204,9 +162,7 @@ const Favorites = () => {
 																		'Added ' +
 																			item.data.name +
 																			' to cart with quantity 1',
-																		{
-																			variant: 'success',
-																		}
+																		{ variant: 'success' }
 																	);
 																}
 															}}
@@ -218,24 +174,15 @@ const Favorites = () => {
 																!item.data.active
 															}
 														>
-															<span className='hidden tooltip lg:flex w-fit rounded shadow-lg p-2 bg-slate-200 text-gray-600 -mt-14 -mr-16'>
-																{!cartItems.some(
-																	(cartItem) => cartItem._id === item.data._id
-																) ? (
-																	<p>Add to Cart</p>
-																) : (
-																	<p>Already in cart</p>
-																)}
-															</span>
 															<MdAddShoppingCart size={27} />
 														</button>
-													</div>
-												</div>
+													</>
+												)}
 											</div>
 										</div>
-									)}
-								</div>
-							))}
+									))}
+							</div>
+						</div>
 					</div>
 				)}
 			</div>
