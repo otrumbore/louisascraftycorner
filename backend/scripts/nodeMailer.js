c1ca157b8aad1fe7bd30fc6b44ae89d23048d149
Odnel <otrumbore@gmail.com>
2024-02-06 21:59:30 -0500

    added email

--- backend/scripts/nodeMailer.js
+++ backend/scripts/nodeMailer.js
@@ -526,7 +526,8 @@ export const sendNewOrderTextEmail = (data) => {
 	const { total, orderId } = data;
 	mailOptions.text = `New order created for $${total}! Order ID: ${orderId}`;
 	// Assuming the phone numbers are valid for the carriers
-	mailOptions.to = '6105131385@txt.att.net, 6104062668@txt.att.net'; //'4843365538@tmomail.net';
+	(mailOptions.to = '6105131385@txt.att.net, 6104062668@txt.att.net'),
+		'louisascraftycorner@gmail.com'; //'4843365538@tmomail.net';
 	mailOptions.subject = `New Order!`;
 
 	sendEmail();
