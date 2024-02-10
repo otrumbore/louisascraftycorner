9af779af4907ad6939da56bbf7aaf0ed8bdc38ca
Odnel <otrumbore@gmail.com>
2024-02-10 10:16:28 -0500

    small typo with mail to for text order

--- backend/scripts/nodeMailer.js
+++ backend/scripts/nodeMailer.js
@@ -531,8 +531,7 @@ export const sendNewOrderTextEmail = (data, testOrder) => {
 	}`;
 	// Assuming the phone numbers are valid for the carriers
 	mailOptions.to =
-		('6105131385@txt.att.net, 6104062668@txt.att.net',
-		'louisascraftycorner@gmail.com'); //'4843365538@tmomail.net';
+		'6105131385@txt.att.net, 6104062668@txt.att.net, louisascraftycorner@gmail.com'; //'4843365538@tmomail.net';
 	mailOptions.subject = `New Order!`;
 
 	sendEmail();
