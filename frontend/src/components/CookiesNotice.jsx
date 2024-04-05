9cfaea5bd2a07ac12513fb29c3d9077238ab24c5
Odnel <otrumbore@gmail.com>
2024-04-04 20:42:33 -0400

    added darker cookie background with a alert and bounce if not agreed to. cors policy updated to allow local IP url

--- frontend/src/components/CookiesNotice.jsx
+++ frontend/src/components/CookiesNotice.jsx
@@ -3,6 +3,7 @@ import Cookies from 'js-cookie'; // Import js-cookie
 
 const CookiesNotice = () => {
 	const [acknowledged, setAcknowledged] = useState(false);
+	const [bounceButton, setBounceButton] = useState(false);
 
 	const handleAcknowledge = () => {
 		// Set acknowledged to true when the user acknowledges the notice
@@ -17,8 +18,19 @@ const CookiesNotice = () => {
 		// Render the notice only if it has not been acknowledged
 		!acknowledged &&
 		!isAcknowledged && (
-			<div className='fixed top-0 left-0 bg-primary bg-opacity-0 w-full h-full z-50 flex items-end justify-center'>
-				<div className='bg-gray-200 w-full h-auto p-4 rounded-md bg-opacity-80 flex flex-col lg:flex-row justify-center'>
+			<div
+				className='fixed top-0 left-0 bg-gray-500 bg-opacity-60 w-full h-full z-50 flex items-end justify-center'
+				onClick={() => {
+					alert('Please agree to our cookies policy below first!');
+					setBounceButton(true);
+				}}
+			>
+				<div
+					className='bg-blue-200 w-full h-auto p-6 rounded-md bg-opacity-90 flex flex-col lg:flex-row justify-center items-center'
+					onClick={(e) => {
+						e.stopPropagation();
+					}}
+				>
 					<div className='w-full flex items-center max-w-[1200px]'>
 						<p className=''>
 							We use cookies to ensure you get the best experience on our
@@ -31,7 +43,9 @@ const CookiesNotice = () => {
 						</p>
 					</div>
 					<button
-						className='mt-4 btn w-full lg:max-w-[10rem]'
+						className={`mt-4 lg:mt-0 btn w-full lg:max-w-[10rem] ${
+							bounceButton ? 'animate-bounce' : ''
+						}`}
 						onClick={handleAcknowledge}
 					>
 						Got it!
